fde4ad52dbb850f3730fd7210d9d2de18b8c5198
Ana-Gutman <ana.gutman2001@gmail.com>
2025-06-05 04:37:39 +0000

    feat(model): add TodoItem entity and relation to TodoList

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+    public long TodoListId { get; set; }
+}
